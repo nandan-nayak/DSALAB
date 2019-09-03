683e6340fc13006cbf4fcf06cbf3391f6f9efea2
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2019-09-03 16:50:59 +0530

    Add files via upload

---
+++ stackvalue/stack.c
@@ -0,0 +1,129 @@
+#include<assert.h>
+#include<stddef.h>
+#include "stack.h"
+
+Stack stack_new(uint32_t size)
+{
+size=(size>0 && size<MAX_DEPTH) ? size : MAX_DEPTH;
+Stack s={size,-1,{0}};
+return s;
+
+}
+
+uint32_t stack_full(const Stack stk)
+{
+
+assert(stk.size!=0);
+return ((stk.top+1)==stk.size);
+}
+
+
+
+uint32_t stack_empty(const Stack stk)
+{
+
+assert(stk.size!=0);
+return stk.top==-1;
+
+
+}
+
+
+
+
+
+
+
+
+
+Stack stack_push(Stack stk,float data,StackResult *result)
+{
+
+assert(stk.size!=0);
+if(stk.top+1<stk.size)
+{
+stk.data[++stk.top]=data;
+result->data=data;
+result->status=STACK_OK;
+
+}
+
+else
+{
+
+result->data=data;
+result->status=STACK_FULL;
+
+}
+
+return stk;
+}
+
+
+
+
+
+
+Stack stack_pop(Stack stk,StackResult *result)
+{
+assert(stk.size!=0);
+
+
+if(stk.top>-1)
+{
+
+result->data=stk.data[stk.top];
+result->status=STACK_OK;
+stk.top--;
+
+}
+else
+{
+result->status=STACK_EMPTY;
+}
+
+return stk;
+}
+
+
+
+
+
+
+
+
+
+
+Stack stack_peek(Stack stk,StackResult *result)
+{
+assert(stk.size!=0);
+if(stk.top>-1)
+{
+
+result->data=stk.data[stk.top];
+result->status=STACK_OK;
+
+}
+
+else
+{
+result->status=STACK_EMPTY;
+
+}
+
+return stk;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
